281b691bae706928d5916b2eb03c9e1538d6f826
victorjaquez <victorajaquez@gmail.com>
2018-10-12 16:12:34 -0700

    update and delete routes

--- server/app.py
+++ server/app.py
@@ -63,6 +63,35 @@ def all_customers():
         response_object['customers'] = CUSTOMERS
     return jsonify(response_object)
 
+# update route
+
+
+@app.route('/customers/<customer_id>', methods=['PUT'])
+def update_customer(customer_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_customer(customer_id)
+        CUSTOMERS.append({
+            'id': uuid.uuid4().hex,
+            'name': post_data.get('name'),
+            'email': post_data.get('email'),
+            'phone': post_data.get('phone'),
+            'contacted': post_data.get('contacted')
+        })
+        response_object['message'] = 'Customer updated!'
+    return jsonify(response_object)
+
+# delete route
+
+
+@app.route('/customers/<customer_id>', methods=['DELETE'])
+def delete_customer(customer_id):
+    response_object = {'status': 'success'}
+    if request.method == 'DELETE':
+        remove_customer(customer_id)
+        response_object['message'] = 'Customer removed!'
+    return jsonify(response_object)
 
 # helper function
 
